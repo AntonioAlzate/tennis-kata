542b014ece1b1906df6af701e67fb65efce8ce5d
Antonio Alzate <antonioalzate7@gmail.com>
2021-12-18 15:15:25 -0500

    Refactorización TennisGame3

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,36 +1,68 @@
+import java.util.Objects;
 
 public class TennisGame3 implements TennisGame {
-    
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    private int pointsPlayer2;
+    private int pointsPlayer1;
+    private final String player1Name;
+    private final String player2Name;
+
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        String score;
+
+        if (noWinNoAdvantageNoDeuce()) {
+            String[] points = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+            score = points[pointsPlayer1];
+            score = getScoreNoWinNoAdvantage(score, points);
         }
-    }
-    
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
+        else if(pointsPlayer1 == pointsPlayer2)
+            score = "Deuce";
         else
-            this.p2 += 1;
-        
+            score = getScoreAdvantageOrWin();
+
+        return score;
+    }
+
+    private boolean noWinNoAdvantageNoDeuce() {
+        return menosDeCuatroPuntos() && noSeConvirtieronSeisPuntos();
+    }
+
+    private String getScoreAdvantageOrWin() {
+        return existAdvantage() ? "Advantage " + getOlderName() : "Win for " + getOlderName();
+    }
+
+    private String getOlderName() {
+        return pointsPlayer1 > pointsPlayer2 ? player1Name : player2Name;
+    }
+
+    private boolean existAdvantage() {
+        int diference = pointsPlayer1 - pointsPlayer2;
+        return diference * diference == 1;
+    }
+
+    private String getScoreNoWinNoAdvantage(String score, String[] points) {
+        return (pointsPlayer1 == pointsPlayer2) ? score + "-All" : score + "-" + points[pointsPlayer2];
+    }
+
+    private boolean noSeConvirtieronSeisPuntos() {
+        return ((pointsPlayer1 + pointsPlayer2) != 6);
+    }
+
+    private boolean menosDeCuatroPuntos() {
+        return pointsPlayer1 < 4 && pointsPlayer2 < 4;
+    }
+
+    public void wonPoint(String playerName) {
+        if (Objects.equals(playerName, "player1"))
+            this.pointsPlayer1 += 1;
+        else
+            this.pointsPlayer2 += 1;
+
     }
 
 }
